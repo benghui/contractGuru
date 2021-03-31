0980feabc6b2b03f039458ded5a69e37de20b5f6
benghui <benghui@gmail.com>
2021-03-31 11:36:59 +0800

    Update request action query & model

--- api/pkg/models/action.go
+++ api/pkg/models/action.go
@@ -1,6 +1,8 @@
 package models
 
 type Action struct {
-	ActionID    int `json:"action_id"`
+	ActionName    string `json:"action_name"`
 	FinanceFlag int `json:"finance_flag"`
+	TransitionID int `json:"transition_id"`
+	EndStateID int `json:"end_state_id"`
 }
